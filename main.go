36de59fd2567b53b303ce0f46b075693ffb36cd4
Yukichi <naginoyukichi@gmail.com>
2023-07-02 22:44:17 +0900

    golang formapp

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 import(
 	"log"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 func main() {
 	router:= gin.Default()
+
+	// 自動的にファイルを返す設定
+	router.StaticFS("/static", http.Dir("static"))
+
+	// ルートなら/static/index.htmlにリダイレクト
 	router.GET("/",func(ctx *gin.Context){
-		ctx.String(200,"Hello, World")
+		ctx.Redirect(302,"/static/index.html")
 	})
+
+	// フォームの内容を受け取って挨拶する
+	router.GET("/hello", func(ctx *gin.Context){
+		name := ctx.Query("name")
+		ctx.Header("Context-Type", "text/html; charset=UTF-8")
+		ctx.String(200, "<h1>Hello, "+ name + "</h1>")
+	})
+
+	// サーバーを起動
 	err := router.Run("127.0.0.1:8888")
 	if err != nil {
 		log.Fatal("サーバー起動に失敗", err)
